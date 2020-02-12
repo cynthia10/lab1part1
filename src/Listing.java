6eb52e9b58ff8e5850ef02b89d9badb2f2b97244
cynthia10 <35684320+cynthia10@users.noreply.github.com>
2020-02-11 19:45:29 -0500

    Initial commit

---
+++ src/Listing.java
@@ -0,0 +1,11 @@
+public class Listing {
+    public static void main(String[]args)
+    {
+      name c =new name();
+      c.getAge();
+      c.getname();
+      c.setAge();
+      c.setName();
+
+    }
+}
---
+++ src/name.java
@@ -0,0 +1,42 @@
+public class name {
+     public String name;
+    private int Age;
+
+    name(String a,int b)
+    {
+        this.Age=Age;
+        this.name=name;
+    }
+
+    public name() {
+
+    }
+
+    public void input(int b,String a)
+    {
+
+    }
+    public void setName(String)
+    {
+        this.name="";
+    }
+
+    public void setAge()
+    {
+    }
+    public int getAge()
+    {
+        return this.Age;
+    }
+    public String getname()
+    {
+        return this.name;
+    }
+    public String toString()
+    {
+
+        return null;
+    }
+
+}
+
